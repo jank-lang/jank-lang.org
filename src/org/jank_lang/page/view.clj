466edd0a5ae2a93a8ba497cd558598ae53271286
jeaye <contact@jeaye.com>
2022-07-03 17:53:09 -0700

    Add noscript support for matomo

--- src/org/jank_lang/page/view.clj
+++ src/org/jank_lang/page/view.clj
@@ -52,17 +52,17 @@
     "<!-- Matomo -->
     <script>
     var _paq = window._paq = window._paq || [];
-    /* tracker methods like 'setCustomDimension' should be called before 'trackPageView' */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-                 var u='//matomo.jeaye.com/';
+                 var u=\"//matomo.jeaye.com/\";
                  _paq.push(['setTrackerUrl', u+'matomo.php']);
                  _paq.push(['setSiteId', '1']);
                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
                  })();
     </script>
+    <noscript><p><img src=\"//matomo.jeaye.com/matomo.php?idsite=1&amp;rec=1\" style=\"border:0;\" alt=\"\" /></p></noscript>
     <!-- End Matomo Code -->"
     (conj (into [:body] body)
           [:footer {:class "footer"}
