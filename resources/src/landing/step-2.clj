03dff9a4b0331df3c458a1f4f87a8a6f8ac7c252
jeaye <contact@jeaye.com>
2023-09-08 10:21:03 -0700

    Update interop example

--- resources/src/landing/step-2.clj
+++ resources/src/landing/step-2.clj
@@ -2,8 +2,9 @@
   (native/raw "__value = make_box(glCreateShader(GL_VERTEX_SHADER));"))
 
 (defn set-shader-source! [shader source]
-  (native/raw "auto const s(detail::to_string(~{ source }));
-               glShaderSource(detail::to_int(~{ shader }), 1, &s.data, nullptr);"))
+  (native/raw "auto const shader(detail::to_int(~{ shader }));
+               auto const &source(detail::to_string(~{ source }));
+               __value = make_box(glShaderSource(shader, 1, &source.data, nullptr));"))
 
 (defn compile-shader! [shader]
-  (native/raw "glCompileShader(detail::to_int(~{ shader }));"))
+  (native/raw "__value = make_box(glCompileShader(detail::to_int(~{ shader })));"))
