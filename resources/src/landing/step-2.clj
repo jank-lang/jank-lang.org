c0b301dedee3dfe69e67fb83c93f5127c76e6b4e
jeaye <contact@jeaye.com>
2025-01-06 18:22:55 -0800

    Update interop syntax

--- resources/src/landing/step-2.clj
+++ resources/src/landing/step-2.clj
@@ -1,10 +1,10 @@
 (defn create-vertex-shader! []
-  (native/raw "__value = make_box(glCreateShader(GL_VERTEX_SHADER));"))
+  (c++/glCreateShader c++/GL_VERTEX_SHADER))
 
 (defn set-shader-source! [shader source]
-  (native/raw "auto const shader(detail::to_int(~{ shader }));
-               auto const &source(detail::to_string(~{ source }));
-               __value = make_box(glShaderSource(shader, 1, &source.data, nullptr));"))
+  (let [shader (int shader)
+        source (str source)]
+    (c++/glShaderSource shader 1 source c++/nullptr)))
 
 (defn compile-shader! [shader]
-  (native/raw "__value = make_box(glCompileShader(detail::to_int(~{ shader })));"))
+  (c++/glCompileShader (int shader)))
