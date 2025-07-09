3311bbf367ad13a30b38ef9ef6bfd6f99878d993
jeaye <contact@jeaye.com>
2025-07-09 15:09:16 -0700

    Fix first example

--- resources/src/landing/step-1.clj
+++ resources/src/landing/step-1.clj
@@ -1,5 +1,5 @@
 (defn -main [& args]
   (loop [game-state (new-game!)]
-    (when (done? game-state)
+    (if (done? game-state)
       (end-game! game-state)
       (recur (next-state game-state)))))
